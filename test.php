29c6477eda77b95d70617f6392605c31d4615fe1
Ruben Ortiz <rubenortizg@gmail.com>
2017-06-04 00:53:32 -0500

    CSS

---
+++ test.php
@@ -0,0 +1,46 @@
+<?php
+
+//array
+$semana = array('Lunes','Martes','Miercoles','Jueves','Viernes','Sabado','Domingo');
+echo $semana[2];
+
+//recorrer Array
+$meses = array(
+  'Enero',
+  'Febrero',
+  'Marzo',
+  'Abril',
+  'Mayo',
+  'Junio',
+  'Julio',
+  'Agosto',
+  'Septiembre',
+  'Octubre',
+  'Noviembre',
+  'Diciembre'
+);
+
+sort($meses);
+?>
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Meses del año</title>
+  </head>
+  <body>
+    <h1>Meses del año</h1>
+    <ul>
+      <?php
+
+      foreach ($meses as $mes) {
+        echo '<li>'.$mes.'</li>';
+      }
+
+      ?>
+
+
+    </ul>
+  </body>
+</html>
